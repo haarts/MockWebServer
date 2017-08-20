752e8b96ce2456643e8ec76f0f0a3f5abc2789fa
Miguel Castiblanco <miguel@starcarr.co>
2017-08-20 12:42:06 -0700

    [Tests] Improved the tests to cover a recently fixed bug

--- test/server_test.dart
+++ test/server_test.dart
@@ -104,6 +104,10 @@ void main() {
           ..body = "working";
       } else if (request.uri.path == "/users/1") {
         return new MockResponse()..httpCode = 201;
+      } else if (request.uri.path == "/delay") {
+        return new MockResponse()
+          ..httpCode = 200
+          ..delay = new Duration(milliseconds: 1500);
       }
 
       return new MockResponse()..httpCode = 404;
@@ -120,6 +124,13 @@ void main() {
 
     response = await _get("users/1");
     expect(response.statusCode, 201);
+
+    Stopwatch stopwatch = new Stopwatch()..start();
+    response = await _get("delay");
+    stopwatch.stop();
+    expect(stopwatch.elapsed.inMilliseconds,
+        greaterThanOrEqualTo(new Duration(milliseconds: 1500).inMilliseconds));
+    expect(response.statusCode, 200);
   });
 
   test("Enqueue MockResponse", () async {
@@ -140,8 +151,7 @@ void main() {
 
   test("Delay", () async {
     _server.enqueue(delay: new Duration(seconds: 2), httpCode: 201);
-    Stopwatch stopwatch = new Stopwatch();
-    stopwatch.start();
+    Stopwatch stopwatch = new Stopwatch()..start();
     HttpClientResponse response = await _get("");
 
     stopwatch.stop();
